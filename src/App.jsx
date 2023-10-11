743537dfcf1eafbb210ac185ff0c79a6b43fb47e
Mehar Ali <mehar0alii@gmail.com>
2023-10-11 12:53:01 +0500

    changing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Variable from './components/Variable'
 
 const App = () => {
   return (
-    <div className='bg-gray-900 p-10  flex flex-col justify-center items-center min-h-screen'>
+    <div className='bg-gray-900 p-2  flex flex-col justify-center items-center min-h-screen'>
       <Variable />
     </div>
   )
